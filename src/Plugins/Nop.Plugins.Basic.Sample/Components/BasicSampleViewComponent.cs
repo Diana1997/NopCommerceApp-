10d02a750eb43e08f69768aa793077304a669456
Diana <israelyan.diana@gmail.com>
2023-03-15 22:42:31 +0400

    Create plugin

---
+++ src/Plugins/Nop.Plugins.Basic.Sample/Components/BasicSampleViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugins.Basic.Sample.Components
+{
+    public class BasicSampleViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Plugins/Basic.Sample/Views/BasicSample.cshtml");
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugins.Basic.Sample/SamplePlugin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugins.Basic.Sample
+{
+    public class SamplePlugin  : BasePlugin, IWidgetPlugin
+    {
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+
+        
+
+        public IList<string> GetWidgetZones()
+        {
+            return new List<string> { PublicWidgetZones.HomepageBeforeNews };
+        }
+
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "BasicSample";
+        }
+
+        public bool HideInWidgetList => false;
+    } 
+}
